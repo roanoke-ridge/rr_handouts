fc30d26e0c4c449281411aea1c184d4f07a0ec87
Rob Danielson <31169893+rawb0t@users.noreply.github.com>
2024-10-29 17:53:40 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ lua54 'yes'
 game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-author 'rob@roanokeridge.com'
+author 'discord:r0bbot'
 
 files {
   'locales/*.json',
